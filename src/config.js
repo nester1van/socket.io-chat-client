6ef1565b3aeec2586d578e5441cfbaaaef371f4b
1-front-end-developer <onefamily2@yandex.ru>
2020-08-15 22:27:21 +0400

    test process.env.REACT_APP_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  SERVER: process.env.CONFIG_ENDPOINT || 'localhost:5000'
+  SERVER: process.env.REACT_APP_ENDPOINT || 'localhost:5000'
 };
 // 'socket-io-chat-server-1.herokuapp.com'
 export default config;
