9ce6458d98f00ff7f902a9eb8c9e1e8d86d061f9
1-front-end-developer <onefamily2@yandex.ru>
2020-08-15 21:45:18 +0400

    test process.env.ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  SERVER: process.env.PORT || 'localhost:5000'
+  SERVER: process.env.ENDPOINT || 'localhost:5000'
 };
 // 'socket-io-chat-server-1.herokuapp.com'
 export default config;
